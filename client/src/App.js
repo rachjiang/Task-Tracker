38afc29be2d2b8fe6b1148b46f44bdb3e378fdcb
rachjiang <rjiang521@gmail.com>
2023-02-23 02:44:50 -0500

    listing all todos and started add component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "./App.css";
+import ListTodos from './components/ListTodos';
+import InputTodo from "./components/AddTodo";
 
 function App() {
   return (
-    <div className="App">
-      {/* insert components here */}
-    </div>
+      <Fragment>
+        <div className="container">
+          <ListTodos />
+          <InputTodo />
+        </div>
+      </Fragment>
   );
 }
 
