61a5288c901140648e1f4720cb7f340c51b054f7
rachjiang <rjiang521@gmail.com>
2023-02-24 12:37:26 -0500

    changed server port for deploy

--- server/index.js
+++ server/index.js
@@ -73,6 +73,6 @@ app.delete("/todos/:id", async(req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log("server has started on port 5000");
+app.listen(3000, () => {
+    console.log("server has started on port 3000");
 });
