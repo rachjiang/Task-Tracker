b98b17f6427095d216f2ce1369a2c17d9a965721
rachjiang <rjiang521@gmail.com>
2023-02-25 01:29:29 -0500

    removed cors config options

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ const db = require("./db"); // allows queries with postgres
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
-
-// allow requests from other domains
-const corsOptions = {
-    origin: "*",
-  };
  
 // middleware to grab data from the client
-app.use(cors(corsOptions)); 
+app.use(cors()); 
 app.use(express.json()) // access to req.body to use json data in api calls
 
 if (process.env.NODE_ENV === "production") {
