b65ebadad231cd11a052f704a28effc4fbf263ab
rachjiang <rjiang521@gmail.com>
2023-02-24 23:01:14 -0500

    testing heroku pg db bug

--- db.js
+++ db.js
@@ -4,18 +4,28 @@ const dotenv = require("dotenv");
 // set up environment variables
 dotenv.config();
 
-const devConfig = {
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+// if in production mode on heroku, we need to pass in the heroku DATABASE_URL. if in dev mode, run local db
+  db = new Pool({
+    // value comes from heroku pg URL value for db named DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+// in dev mode, pass env variables
+} else {
+  db = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT
-};
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
+  });
+}
 
-// eslint-disable-next-line no-unused-vars
-const proConfig = process.env.DATABASE_URL // value comes from heroku pg URL value for db named DATABASE_URL
 
-const db = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
-// if in production mode on heroku, we need to pass in the heroku DATABASE_URL. if in dev mode, run local db
+
+
 
 module.exports = db;
