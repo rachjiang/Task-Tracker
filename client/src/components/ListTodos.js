82e218ac4da3ff794d27852383ef7d3adb383b37
rachjiang <rjiang521@gmail.com>
2023-02-24 23:16:00 -0500

    testing

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -60,7 +60,7 @@ function ListTodos () {
                     <tbody>
                     {todos.map(todo => (
                         <tr key={todo.todo_id}>
-                            <td><input className="mx-5" type="checkbox" checked={todo.completed} onChange={() => handleCheckboxChange(todo.todo_id)} /></td>
+                            <td><input className="mx-5" type="checkbox" checked={todo.completed} onChange={(event) => handleCheckboxChange(todo.todo_id)} /></td>
                             <td><Link to={`/todos/${todo.todo_id}`}>{todo.description}</Link></td>
                             <td><EditTodo todo={todo}/></td>
                             <td><button type="submit" className="btn btn-danger" onClick={() => handleDelete(todo.todo_id)}>Delete</button></td>
